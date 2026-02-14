5287cb453353492101c7e190c368a82155821481
zikemstephane <162482937+zikemstephane@users.noreply.github.com>
2026-02-14 02:11:21 +0100

    CRUD vehicule,course et note complet avec dashboard v1

--- pategou/settings.py
+++ pategou/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,6 +80,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     #'authentication.middleware.LoginRequiredMiddleware',
 ]
 
@@ -165,6 +167,7 @@ EMAIL_HOST_PASSWORD = 'znghyeokurdjdrea'
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
